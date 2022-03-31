0b905ce47ce8342cfb5f8dcc7aaec91d5b40692f
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-31 20:11:55 +0900

    페이지 특정 위치로 이동

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import * as S from "../style/header.ts";
 import Logo from "../img/Logo.svg";
+import { Link } from "react-scroll";
 
 interface props {
   station: string;
@@ -8,11 +9,21 @@ interface props {
 const Header = ({ station }: props) => {
   return (
     <S.Header>
-      <S.Logo src={Logo} />
-      <S.Title>
-        <p className="title">지금 한강 온도는</p>
-        <p className="subTitle">{station && `${station} 한강물 온도`}</p>
-      </S.Title>
+      <div>
+        <S.Logo src={Logo} />
+        <S.Title>
+          <p className="title">지금 한강 온도는</p>
+          <p className="subTitle">{station && `${station} 한강물 온도`}</p>
+        </S.Title>
+      </div>
+      <S.Nav>
+        <Link to="temp" spy={true} smooth={true}>
+          <p>temp</p>
+        </Link>
+        <Link to="explanation" spy={true} smooth={true}>
+          <p>explanation</p>
+        </Link>
+      </S.Nav>
     </S.Header>
   );
 };
