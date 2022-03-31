7b4e66b923291fc7a486bbe242d0a8d428c4a688
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-31 15:51:02 +0900

    스크롤하여 내렸을 때 이미지가 화면에 fadeIn

--- src/components/detailExplanation.tsx
+++ src/components/detailExplanation.tsx
@@ -1,7 +1,18 @@
 import * as S from "../style/detailExplanation";
+import hangang from "../img/hangang.jpeg";
+import Fade from "react-reveal/Fade";
 
 const DetailExplanation = () => {
-  return <S.Page></S.Page>;
+  return (
+    <S.Page>
+      <S.HangangImg>
+        <Fade right big>
+          <img src={hangang} alt="한강 이미지" />
+        </Fade>
+      </S.HangangImg>
+      <S.ContextDiv></S.ContextDiv>
+    </S.Page>
+  );
 };
 
 export default DetailExplanation;
--- src/style/detailExplanation.ts
+++ src/style/detailExplanation.ts
@@ -3,4 +3,27 @@ import styled from "styled-components";
 export const Page = styled.main`
   height: 100vh;
   width: 100vw;
+  display: flex;
+  background-color: #03005f;
+
+  div {
+    padding-top: 10vh;
+    height: 90vh;
+  }
+`;
+
+export const HangangImg = styled.div`
+  width: 40%;
+  align-items: center;
+  display: flex;
+  img {
+    margin: 0 5%;
+    width: 90%;
+    border-radius: 5px;
+    border: 3px solid #fff;
+  }
+`;
+
+export const ContextDiv = styled.div`
+  width: 60%;
 `;
